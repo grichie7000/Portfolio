3440b0a79ffe5afac74b55a32dd46ade7b9c65b8
grichie7000 <richycatchem@gmail.com>
2025-05-27 16:54:43 +0200

    import pages

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { LandingPageComponent } from './landing-page/landing-page.component';
+import { LegalNoticeComponent } from './legal-notice/legal-notice.component';
+import { PrivacyPolicyComponent } from './privacy-policy/privacy-policy.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '', component:LandingPageComponent},
+    {path: 'legal-notice', component:LegalNoticeComponent},
+    {path: 'privacy-policy', component:PrivacyPolicyComponent}
+];
