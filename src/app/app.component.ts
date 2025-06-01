77c8d218cdbbf3c7ceb616b5a21ad0bf82f55c2d
grichie7000 <richycatchem@gmail.com>
2025-06-01 13:07:03 +0200

    why-me

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import AOS from 'aos';
 
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'portfolio';
+
+  ngOnInit() {
+    AOS.init();
+  }
 }
--- src/app/landing-page/landing-page.component.ts
+++ src/app/landing-page/landing-page.component.ts
@@ -1,10 +1,11 @@
 import { Component} from '@angular/core';
 import { StickyMenuComponent } from '../sticky-menu/sticky-menu.component';
 import { HeroComponent } from '../hero/hero.component';
+import { WhyMeComponent } from '../why-me/why-me.component';
 
 @Component({
   selector: 'app-landingpage',
-  imports:[StickyMenuComponent, HeroComponent],
+  imports:[StickyMenuComponent, HeroComponent, WhyMeComponent],
   templateUrl: './landing-page.component.html',
   styleUrls: ['./landing-page.component.scss']
 })
