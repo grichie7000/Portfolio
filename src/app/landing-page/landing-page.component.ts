994042cca88e1b07a097c23fb275013c480b3ac6
grichie7000 <richycatchem@gmail.com>
2025-06-02 09:14:55 +0200

    initial my-skils

--- src/app/landing-page/landing-page.component.ts
+++ src/app/landing-page/landing-page.component.ts
@@ -2,10 +2,11 @@ import { Component} from '@angular/core';
 import { StickyMenuComponent } from '../sticky-menu/sticky-menu.component';
 import { HeroComponent } from '../hero/hero.component';
 import { WhyMeComponent } from '../why-me/why-me.component';
+import { MySkillsComponent } from "../my-skills/my-skills.component";
 
 @Component({
   selector: 'app-landingpage',
-  imports:[StickyMenuComponent, HeroComponent, WhyMeComponent],
+  imports: [StickyMenuComponent, HeroComponent, WhyMeComponent, MySkillsComponent],
   templateUrl: './landing-page.component.html',
   styleUrls: ['./landing-page.component.scss']
 })
