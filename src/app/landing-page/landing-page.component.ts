1a7eb92fd9ae92cab7f890178cb8f01dc8c7a4b5
grichie7000 <richycatchem@gmail.com>
2025-06-07 08:50:18 +0200

    teamplayer styling

--- src/app/landing-page/landing-page.component.ts
+++ src/app/landing-page/landing-page.component.ts
@@ -4,10 +4,11 @@ import { HeroComponent } from '../hero/hero.component';
 import { WhyMeComponent } from '../why-me/why-me.component';
 import { MySkillsComponent } from "../my-skills/my-skills.component";
 import { MyProjectsComponent } from '../my-projects/my-projects.component';
+import { TeamplayerComponent } from '../teamplayer/teamplayer.component';
 
 @Component({
   selector: 'app-landingpage',
-  imports: [StickyMenuComponent, HeroComponent, WhyMeComponent, MySkillsComponent, MyProjectsComponent],
+  imports: [StickyMenuComponent, HeroComponent, WhyMeComponent, MySkillsComponent, MyProjectsComponent, TeamplayerComponent],
   templateUrl: './landing-page.component.html',
   styleUrls: ['./landing-page.component.scss']
 })
