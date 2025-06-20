cf7a7b1f3294d47088173be3b127c87cb3bdb390
grichie7000 <richycatchem@gmail.com>
2025-06-20 09:17:32 +0200

    responisv

--- src/app/my-skills/my-skills.component.ts
+++ src/app/my-skills/my-skills.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-my-skills',
@@ -7,4 +7,29 @@ import { Component } from '@angular/core';
   templateUrl: './my-skills.component.html',
   styleUrls: ['./my-skills.component.scss'],
 })
-export class MySkillsComponent {}
+export class MySkillsComponent implements OnInit {
+  isMobile: boolean = false;
+  isTouchDevice: boolean = false;
+
+  ngOnInit(): void {
+    this.checkScreenSize();
+    this.detectTouchDevice(); 
+  }
+
+  @HostListener('window:resize', [])
+  onResize(): void {
+    this.checkScreenSize();
+  }
+
+  private checkScreenSize(): void {
+    this.isMobile = window.innerWidth <= 1020;
+  }
+
+  private detectTouchDevice(): void {
+    this.isTouchDevice = 
+      'ontouchstart' in window || navigator.maxTouchPoints > 0;
+      console.log(this.isTouchDevice);
+      
+  }
+}
+
--- src/app/sticky-menu/sticky-menu.component.ts
+++ src/app/sticky-menu/sticky-menu.component.ts
@@ -1,11 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-sticky-menu',
   imports: [],
   templateUrl: './sticky-menu.component.html',
-  styleUrl: './sticky-menu.component.scss'
+  styleUrl: './sticky-menu.component.scss',
 })
 export class StickyMenuComponent {
+  constructor() {}
+  isMenuOpen = false;
 
+  toggleMenu() {
+    this.isMenuOpen = !this.isMenuOpen;
+  }
 }
